24576a832380ad616c79cf1758237698d019554d
Jeffrey Nguyen <jeffnguyen91@hotmail.com>
2017-11-05 17:08:55 -0800

    edit tasklisttest.php

--- tests/TaskListTest.php
+++ tests/TaskListTest.php
@@ -1,23 +1,34 @@
 <?php
-use PHPUnit\Framework\TestCase;
-if (!class_exists('\PHPUnit\Framework\TestCase') &&
-    class_exists('\PHPUnit_Framework_TestCase')) {
-    class_alias('\PHPUnit_Framework_TestCase', 'PHPUnit\Framework\TestCase');
-}
-class TaskListTest extends TestCase {
+   use PHPUnit\Framework\TestCase;
+   class TaskListTest extends TestCase
+  {  
+  
     private $CI;
-    public function setUp() {
-        // Load CI instance normally
+     
+    public function setUp()
+    {
         $this->CI = &get_instance();
+        $this->CI->load->model('Tasks');
     }
-    public function testUncompletedTasksGreaterThanCompleted() {
-        $numOfUncompleted = 0;
-        $tasks = (new Tasks()) -> all();
-        foreach ($tasks as $task) {
-            if ($task->status != 2) {
-                $numOfUncompleted++;
-            }
-        }
-        $this->assertGreaterThan(count($tasks)/2, $numOfUncompleted);
+      
+    
+    public function testMoreRule ()
+    {
+       $tasks = $this->CI->Tasks->all();
+       $countDone = 0;
+       $countTodo = 0;
+       
+       foreach ($tasks as $task)
+       {
+           if($task->status == 2) {
+               $countDone++;
+           } else {
+               $countTodo++;
+           }
+       }
+       
+       $result = ($countTodo - $countDone) > 0 ? true : false;
+           
+       $this->assertEquals(true, $result);
     }
-}
+  }
